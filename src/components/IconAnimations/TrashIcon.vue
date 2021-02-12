b8fdae43cc0fdefce0166dfa208f8d2302672e6d
johnbister <john.bister@ibm.com>
2021-02-12 09:31:08 -0600

    add trash icon

---
+++ src/components/IconAnimations/TrashIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg class="icon icon--trash" aria-hidden="true" viewBox="0 -10 36 44">
+    <title>download toolkit</title>
+    <path
+      class="lid"
+      d="M20,4h-8V2h8V4z M4,6v2h24V6H4z"
+    />
+    <path class="bin" d="M14,24h-2V12h2V24z M20,12h-2v12h2V12z M24,8v20H8V8H6v20c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2V8H24z" />
+  </svg>
+</template>
+<script>
+export default {
+  name: "DownloadIcon"
+};
+</script>
+<style lang="scss">
+.icon--download {
+  width: 36px;
+  height: 44px;
+  margin-top: -12px;
+}
+</style>
