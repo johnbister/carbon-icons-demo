d2525dc424fdcbd7912ba100a43930e52ec21a38
johnbister <john.bister@ibm.com>
2021-02-16 07:56:42 -0600

    add upload icon vue

---
+++ src/components/IconAnimations/UploadIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg class="icon icon--upload" aria-hidden="true" viewBox="0 -10 36 44">
+    <title>upload toolkit</title>
+    <path
+      class="arrow"
+      d="M16,2 6,12 7.4,13.4 15,5.8 15,24 17,24 17,5.8 24.6,13.4 26,12z" 
+    />
+    <path class="tray" d="M26 24v4H6v-4H4v4a2 2 0 002 2h20a2 2 0 002-2v-4z" />
+  </svg>
+</template>
+<script>
+export default {
+  name: "UploadIcon"
+};
+</script>
+<style lang="scss">
+.icon--upload {
+  width: 36px;
+  height: 44px;
+  margin-top: -12px;
+}
+</style>
