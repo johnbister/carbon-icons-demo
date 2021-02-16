57c02f9773d42daed480aea26d301be52b1dec70
johnbister <john.bister@ibm.com>
2021-02-16 07:50:05 -0600

    add power icon vue

---
+++ src/components/IconAnimations/PowerIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg class="icon icon--power" aria-hidden="true" >
+    <title>power</title>
+    <rect class="power-line" x="15" y="2" width="2" height="14" />
+    <path
+      class="power--circle"
+      d="M22.5,5.7l-1,1.7c5.3,3,7.1,9.8,4,15s-9.8,7.1-15,4s-7.1-9.8-4-15c1-1.7,2.4-3.1,4-4l-1-1.7C3.3,9.3,1.2,17.3,4.7,23.5
+	    s11.5,8.3,17.8,4.8s8.3-11.5,4.8-17.8C26.1,8.5,24.5,6.9,22.5,5.7z"
+    />
+    <path />
+  </svg>
+</template>
+<script>
+export default {
+  name: "PowerIcon"
+};
+</script>
+<style lang="scss">
+.icon--power {
+  width: 32px;
+  height: 32px;
+}
+</style>
+
