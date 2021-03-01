6ee5bd03e8e550941bba210249abbb5023bcf3a4
johnbister <john.bister@ibm.com>
2021-03-01 08:55:40 -0800

    add save icon vue

---
+++ src/components/IconAnimations/SaveIcon.vue
@@ -0,0 +1,26 @@
+<template>
+  <svg class="icon icon--save" aria-hidden="true" viewBox="0 -10 36 44">
+    <title>save</title>
+    <path
+      class="slide"
+      d="M22,5.7c0,0,0,2.5,0,4.3s-1.8,2-1.8,2s-6.4,0-8.2,0s-2-1.8-2-1.8V6h2v4h8V5.7H22z" 
+    />
+    <path class="base" d="M27.7,9.3l-5-5C22.5,4.1,22.3,4,22,4H6C4.9,4,4,4.9,4,6v20c0,1.1,0.9,2,2,2h20c1.1,0,2-0.9,2-2V10
+		C28,9.7,27.9,9.5,27.7,9.3z M20,26h-8v-8h8V26z M26,26h-4v-8c0-1.1-0.9-2-2-2h-8c-1.1,0-2,0.9-2,2v8H6V6h15.6l4.4,4.4V26z" />
+  </svg>
+</template>
+<script>
+export default {
+  name: "SaveIcon"
+};
+</script>
+<style lang="scss">
+.icon--save {
+  width: 36px;
+  height: 44px;
+  margin-top: -12px;
+}
+</style>
+
+
+
